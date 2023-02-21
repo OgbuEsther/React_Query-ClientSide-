48b405c2c8f7960064bc3bda848bed4fdec4ae33
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 16:45:52 +0100

    saved

--- src/components/api/api.tsx
+++ src/components/api/api.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+interface data {
+  title: string;
+  description: string;
+}
+
 const endPoints = `http://localhost:3400/api/post`;
 
 export const getAll = async () => {
   return await axios.get(`${endPoints}/getposts`).then((res) => res.data);
 };
 
-export const newPost = async () => {
-  return await axios.post(`${endPoints}/createpost`);
+export const newPost = async ({ title, description }: data) => {
+  return await axios.post(`${endPoints}/createpost`, { title, description });
 };
