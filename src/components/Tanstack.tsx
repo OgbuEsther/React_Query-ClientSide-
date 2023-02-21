6c76abc4903f0de3a627be4b99013bc2e9375440
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 14:14:33 +0100

    saved

--- src/components/Tanstack.tsx
+++ src/components/Tanstack.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Tanstack = () => {
-  return <div></div>;
+  return (
+    <div>
+      <center style={{ marginTop: "100px" }}>
+        <input type="text" placeholder="Enter your title" />
+        <br />
+        <input
+          style={{ marginTop: "20px" }}
+          type="text"
+          placeholder="Enter your description"
+        />
+        <br />
+
+        <button style={{ marginTop: "20px" }}>upload</button>
+
+        <div>
+          <h1>view your post below</h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <div
+              style={{
+                width: "300px",
+                height: "300px",
+                backgroundColor: "aliceblue",
+                marginLeft: "30px",
+                marginRight: "30px",
+                boxShadow: "0px 0px 3px 1px rgba(0,0,0,0.1)",
+              }}
+            ></div>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
 };
 
 export default Tanstack;
