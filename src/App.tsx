10e5ed2a73c130d0efb06e2e374235387b6dea6b
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 18:29:46 +0100

    saved

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Tanstack from "./components/Tanstack";
+import YupTest from "./yup/YupTest";
 
 function App() {
   return (
     <div className="App">
-      <Tanstack />
+      {/* <Tanstack /> */}
+      <YupTest />
     </div>
   );
 }
